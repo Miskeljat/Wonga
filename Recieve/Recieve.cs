8ec6cd940dad6d75733e13da66c1b2fd4ac3f98e
Nico <nico.vdmerwe@blablablastudios.com>
2018-05-28 06:35:50 +0200

    Wonga Project
    Wonga Project added

---
+++ Recieve/Recieve.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Recieve
+{
+    class ReceiveMessage
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Name",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                    Console.WriteLine(" [*] PROCESSING...");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine("Received: {0}", "Hello " + message + ", I am your father");
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine(" [*] COMPLETED...");
+
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    channel.BasicConsume(queue: "Name",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine("Press ENTER to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    class SendMessage
+    {
+        public static void Main(string[] args)
+        {           
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Name",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string _name = GetName(args);            
+                       
+                    var body = Encoding.UTF8.GetBytes(_name); 
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "Name",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("Sent: {0}", _name);
+                }
+
+                Console.WriteLine("Press ENTER to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetName(string[] args)
+        {
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Please enter your name:");
+
+            UserName p = new UserName(Console.ReadLine());
+            string _user = p.NameMethod();
+
+            return ((args.Length > 0) ? string.Join("Hello my name is, ", args) : _user);
+        }
+    }
+
+    class UserName
+    {
+        string userName;
+        public string FirstName { get { return userName; } set { userName = value; } }
+
+        public UserName(string _name)
+        {
+            userName = _name;
+        }
+
+        public string NameMethod()
+        {
+            string x = userName;
+            return x;
+        }
+    }
+}
